9249fa05e4cb0cfadd303ac35be81afda58596dc
AndrewDonelson <andrew@nlaak.com>
2019-10-14 07:53:26 -0500

    force travis rebuild

--- golog.go
+++ golog.go
@@ -24,6 +24,7 @@ const (
 	// %[8] // %{message}
 	defProductionFmt  = "%.16[3]s %.19[2]s %.3[7]s ▶ %[8]s"
 	defDevelopmentFmt = "%.16[3]s %.19[2]s %.8[7]s ▶ %[4]s ▶ %[8]s"
+
 	// Error, Fatal, Critical Format
 	//defErrorFmt = "%.16[3]s %.19[2]s %.8[7]s ▶ %[8]s\n▶ %[5]s:%[6]d-%[4]s"
 )
