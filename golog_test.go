f42d3412e456ae3729276cf42ff64a0cc4162a8e
AndrewDonelson <andrew@nlaak.com>
2019-11-10 14:38:37 -0600

    bug: changed line number

--- golog_test.go
+++ golog_test.go
@@ -286,7 +286,7 @@ func TestLogger_SetFormat(t *testing.T) {
 	log.Debug("Test")
 	//log.SetLogLevel(InfoLevel)
 
-	want := fmt.Sprintf("[34m[pkgname] %s DEB - golog_test.go#285-TestLogger_SetFormat - Test[0m\n", time.Now().Format("2006-01-02 15:04:05"))
+	want := fmt.Sprintf("[34m[pkgname] %s DEB - golog_test.go#286-TestLogger_SetFormat - Test[0m\n", time.Now().Format("2006-01-02 15:04:05"))
 	have := buf.String()
 	if have != want {
 		t.Errorf("\nWant: %sHave: %s", want, have)
